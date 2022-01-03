e102a9ddf0dc978bae81f74a7d5e5025997f7fa1
Alfarih Faza <alfarihfz@gmail.com>
2022-01-03 10:50:58 +0700

    feat: added Get opt arg for int32 & int64 args

--- args.go
+++ args.go
@@ -26,6 +26,32 @@ func GetOptIntArg(opts []int, defaultValue ...int) int {
 	return o
 }
 
+func GetOptInt32Arg(opts []int32, defaultValue ...int32) int32 {
+	var o int32 = 0
+	if len(defaultValue) > 0 {
+		o = defaultValue[0]
+	}
+
+	if len(opts) > 0 {
+		o = opts[0]
+	}
+
+	return o
+}
+
+func GetOptInt64Arg(opts []int64, defaultValue ...int64) int64 {
+	var o int64 = 0
+	if len(defaultValue) > 0 {
+		o = defaultValue[0]
+	}
+
+	if len(opts) > 0 {
+		o = opts[0]
+	}
+
+	return o
+}
+
 func GetOptBoolArg(opts []bool, defaultValue ...bool) bool {
 	var o = false
 	if len(defaultValue) > 0 {
